b32b3b0012bcca17cd81597f7b06395a29418fdc
Krish <kvdesai2002@gmail.com>
2022-11-27 12:15:57 +0530

    common.css

--- phone-directory/src/index.js
+++ phone-directory/src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import AddSubscriber from './AddSubscriber';
 import reportWebVitals from './reportWebVitals';
-
+import './common/common.css';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
