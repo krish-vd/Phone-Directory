39549938afa4af3c6191ed731d09eff62fd41edc
Krish <kvdesai2002@gmail.com>
2022-11-27 12:11:12 +0530

    AddSubscriber 1

--- phone-directory/src/index.js
+++ phone-directory/src/index.js
@@ -2,8 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
+import AddSubscriber from './AddSubscriber';
 import reportWebVitals from './reportWebVitals';
-import AddSubscriber from './AddSubscriber'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
