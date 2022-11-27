5519b7b5a6745da9d3507f8692ca49cca9369997
Krish <kvdesai2002@gmail.com>
2022-11-27 11:47:38 +0530

    Manually adding subscriber details

--- phone-directory/src/App.js
+++ phone-directory/src/App.js
@@ -1,12 +1,13 @@
-import React, {Component} from 'react';
-import Header from './Header';
-import './App.css';
+import React, { Component } from "react";
+import Header from "./Header";
+import "./App.css";
 
 class App extends Component {
-  render(){
-  return (
-    <div>
-            <Header />
+  render() {
+    
+    return (
+      <div>
+        <Header />
         <div className="component-body-container">
           <button className="custom-btn add-btn">Add</button>
 
@@ -14,9 +15,17 @@ class App extends Component {
             <span className="grid-item name-heading">Name</span>
             <span className="grid-item phone-heading">Phone</span>
           </div>
-    </div>
-    </div>
-  );
+          <div className="grid-container">
+            <span className="grid-item">Krish</span>
+            <span className="grid-item">7600486767</span>
+          </div>
+          <div className="grid-container">
+            <span className="grid-item">Hetal</span>
+            <span className="grid-item">9428611032</span>
+          </div>
+        </div>
+      </div>
+    );
   }
 }
 
