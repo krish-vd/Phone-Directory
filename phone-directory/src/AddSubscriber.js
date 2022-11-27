c04761e6350b6096ca20fe068531e538f7aa1254
Krish <kvdesai2002@gmail.com>
2022-11-27 12:10:15 +0530

    AddSubscriber file added

---
+++ phone-directory/src/AddSubscriber.js
@@ -0,0 +1,11 @@
+import React, {Component} from 'react';
+
+class AddSubscriber extends Component {
+    render() {
+        return (
+            <div></div>
+        )
+    }
+}
+
+export default AddSubscriber;
