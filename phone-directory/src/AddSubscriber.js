2f04dd9bfe67d7df44cee0215af5729335fe297d
Krish <kvdesai2002@gmail.com>
2022-11-27 12:14:01 +0530

    'Back' button in AddSubscriber

--- phone-directory/src/AddSubscriber.js
+++ phone-directory/src/AddSubscriber.js
@@ -1,9 +1,15 @@
 import React, {Component} from 'react';
+import Header from './Header'
 
 class AddSubscriber extends Component {
     render() {
         return (
-            <div></div>
+            <div>
+                <Header heading="Add Subscriber"/>
+                <div className="component-body-container">
+                    <button className="custom-btn">Back</button>
+                </div>
+            </div>
         )
     }
 }
